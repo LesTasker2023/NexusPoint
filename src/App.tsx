95bdc69191d77900fb737e97f89b30e4eea08136
Les <leseatfire@gmail.com>
2024-04-25 16:50:50 +0100

    feeding data from mock response to Listing Tile

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
-import "./App.css";
-import { Card } from "./components";
+import { ListingTile } from "./components";
+import data from "./utils/data/mock-vehicle-search-response.json";
 
 function App() {
+  console.log("🚀 ~ data:", data);
   return (
-    <Card
-      name={"Test"}
-      plate={"Test"}
-      make={"Test"}
-      model={"Test"}
-      derivative={"Test"}
-      advert_classification={"Test"}
-      fuel_type={"Test"}
-      transmission={"Test"}
+    <ListingTile
+      name={data.data[0].name}
+      plate={data.data[0].plate}
+      make={data.data[0].make}
+      model={data.data[0].model}
+      derivative={data.data[0].derivative}
+      advert_classification={data.data[0].advert_classification}
+      fuel_type={data.data[0].fuel_type}
+      transmission={data.data[0].transmission}
     />
   );
 }
