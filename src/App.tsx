ff0bf9dbe41d14697ad57d6857230fee8dd56ea6
Les <leseatfire@gmail.com>
2024-04-26 07:35:20 +0100

    Tidy up Listing Tile props

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,13 @@ function App() {
   const vehicleDetails = data.data[0];
   return (
     <ListingTile
-      name={vehicleDetails.name}
-      plate={vehicleDetails.plate}
-      make={vehicleDetails.make}
-      model={vehicleDetails.model}
-      derivative={vehicleDetails.derivative}
-      advert_classification={vehicleDetails.advert_classification}
-      fuel_type={vehicleDetails.fuel_type}
-      transmission={vehicleDetails.transmission}
-      media_urls={vehicleDetails.media_urls}
-      price={vehicleDetails.price}
-      original_price={vehicleDetails.original_price}
+      {...vehicleDetails}
       // No data provided in mock response for the below props
       // Hardcoded for visibility of the elements in the ListingTile
       monthly_payment={(parseInt(vehicleDetails.price) / 48)
         .toFixed(2)
         .toString()}
       finance={"(PCP)"}
-      slug={vehicleDetails.slug}
     />
   );
 }
